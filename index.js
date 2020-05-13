2e62b18c469ea022de673d9d19b66565d5a67826
Gurko Ilya <blues74@mail.ru>
2020-05-13 11:20:17 +0300

    buttons

--- index.js
+++ index.js
@@ -2,15 +2,37 @@ function byId$ (id) {
   return $(`#${id}`);
 }
 
+function getButtonWithPopover(val, title, text) {
+  return `<button
+    type="button"
+    data-name="word"
+    class="btn btn-lg btn-outline-primary"
+    style="margin-bottom: .5rem;"
+    data-toggle="popover"
+    title="${title}"
+    data-content="${text}"
+  >${val}</button>
+  `;
+}
+
 function loadWords (id) {
   let arr = wordsAll[id].trim().split('\n');
   let html = '';
+
   arr.forEach((item, i) => {
-    //itemArr = item.split('---');
-    html += item + '<br/>';
+    itemArr = item.split('---').map(item => item.trim());
+    subhtml = getButtonWithPopover(itemArr[0], itemArr[1], itemArr[3] + ' --- ' + itemArr[2] );
+
+    html += subhtml; // + '<br/>';
   });
 
   byId$('current_words').html(html);
+
+  byId$('current_words').find('[data-name="word"]').popover({
+    container: 'body',
+    trigger: 'focus'
+  });
+
 }
 
 function initView () {
