fc8d5add80da856f04fa4842a48b27a2fb1276a7
Gurko Ilya <blues74@mail.ru>
2020-05-13 15:50:40 +0300

    noop

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const options = {
+    traceByTip: false
+}
+
 function byId (id) {
   return $(`#${id}`);
 }
@@ -27,10 +31,17 @@ function loadWords (id) {
   let html = '';
 
   arr.forEach((item, i) => {
-    itemArr = item.split('---').map(item => item.trim());
-    subhtml = getButtonWithPopover(itemArr[0], itemArr[1], itemArr[3] + ' --- ' + itemArr[2] );
-
-    html += subhtml; // + '<br/>';
+    item = item.split('---').map(item => item.trim());
+    const word = item[0];
+    const tip  = item[3].split(' ').shift();
+    const text = item[3] + ' --- ' + item[2];
+    let value = word;
+    let title = item[1] + ' --- ' + tip;
+    if (options.traceByTip) {
+      value = tip;
+      title = word + ' --- ' + item[1];
+    }
+    html += getButtonWithPopover(value, title, text);
   });
 
   byId('current_words').html(html);
@@ -43,6 +54,10 @@ function loadWords (id) {
 }
 
 function initView () {
+  byId('traceByTip').on('click', (e) => {
+    options.traceByTip = e.target.checked;
+  });
+
   byName('goToWords', byId('toolbar')).on('click', (e) => {
     loadWords(e.target.innerText);
   });
