0e6368f9d19180154cdee0c710dd1570cdd9d4f2
Gurko Ilya <blues74@mail.ru>
2020-05-30 07:53:52 +0300

    077

--- math/007.js
+++ math/007.js
@@ -1,30 +1,5 @@
 TEXTS.push([`
 <eng>
-First, I discuss how the number system you’re familiar with — called the Hindu-Arabic number system (or decimal numbers) — uses digits and place value to express numbers.
-Next, I show you how to round numbers to the nearest ten, hundred, or thousand.
-After that, I discuss the Big Four operations: adding, subtracting, multiplying, and dividing.
-You see how to use the number line to make_sense_of all four operations.
-Then I give you practice doing calculations with larger numbers.
-To finish up, I make sure you know how to do long division both with and without a remainder.
-Algebra often uses the dot (.) in place of the times sign (x) to indicate multiplication, so that’s what I use in this book.
-<rus>
-Сначала я расскажу, как система счисления, с которой вы знакомы - она называется индуистско-арабской системой счисления (или десятичными числами) - использует цифры и знаки места для выражения чисел.
-Далее я покажу вам, как округлять числа до ближайших десяти, сотен или тысяч.
-После этого я обсуждаю операции Большой Четверки: сложение, вычитание, умножение и деление.
-Вы видите, как использовать числовую строку, чтобы понять все четыре операции.
-Затем я даю вам практику выполнения расчетов с большими числами.
-Чтобы закончить, я уверен, что вы знаете, как сделать длинное деление как с остатком, так и без него.
-Алгебра часто использует точку (.) Вместо знака времени (x) для обозначения умножения, и именно это я и использую в этой книге.
-<trn>
-fɜːst, aɪ dɪsˈkʌs haʊ ðə ˈnʌmbə ˈsɪstɪm jʊə fəˈmɪliə wɪð — kɔːld ðə ˈhɪnˈduː-ˈærəbɪk ˈnʌmbə ˈsɪstɪm (ɔː ˈdɛsɪməl ˈnʌmbəz) — ˈjuːzɪz ˈdɪʤɪts ænd pleɪs ˈvæljuː tuː ɪksˈprɛs ˈnʌmbəz.
-nɛkst, aɪ ʃəʊ juː haʊ tuː raʊnd ˈnʌmbəz tuː ðə ˈnɪərɪst tɛn, ˈhʌndrəd, ɔː ˈθaʊzənd.
-ˈɑːftə ðæt, aɪ dɪsˈkʌs ðə bɪg fɔːr ˌɒpəˈreɪʃənz: ˈædɪŋ, səbˈtræktɪŋ, ˈmʌltɪplaɪɪŋ, ænd dɪˈvaɪdɪŋ.
-juː siː haʊ tuː juːz ðə ˈnʌmbə laɪn tuː meɪk sɛns ɒv ɔːl fɔːr ˌɒpəˈreɪʃənz.
-ðɛn aɪ gɪv juː ˈpræktɪs ˈdu(ː)ɪŋ ˌkælkjʊˈleɪʃənz wɪð ˈlɑːʤə ˈnʌmbəz.
-tuː ˈfɪnɪʃ ʌp, aɪ meɪk ʃʊə juː nəʊ haʊ tuː duː lɒŋ dɪˈvɪʒən bəʊθ wɪð ænd wɪˈðaʊt ə rɪˈmeɪndə.
-ˈælʤɪbrə ˈɒf(ə)n ˈjuːzɪz ðə dɒt (.) ɪn pleɪs ɒv ðə taɪmz saɪn (ɛks) tuː ˈɪndɪkeɪt ˌmʌltɪplɪˈkeɪʃən, səʊ ðæts wɒt aɪ juːz ɪn ðɪs bʊk.
-
-<eng>
 The number system used most commonly throughout the world is the Hindu-Arabic number system.
 This system contains ten digits (also called numerals), which are symbols like the letters A through Z.
 I’m sure you’re quite familiar with them:
@@ -126,6 +101,10 @@ The first 0 is a leading zero, and the remaining 0s are placeholders.
 ðə fɜːst 0 ɪz ɪn ðə ˈhʌndrəd-ˈθaʊzəndz pleɪs, 4 ɪz ɪn ðə tɛn-ˈθaʊzəndz pleɪs, ðə nɛkst 0 ɪz ɪn ðə ˈθaʊzəndz pleɪs, 1 ɪz ɪn ðə ˈhʌndrədz pleɪs, 2 ɪz ɪn ðə tɛnz pleɪs, ænd ðə lɑːst 0 ɪz ɪn ðə wʌnz pleɪs, səʊ
 ðə fɜːst 0 ɪz ə ˈliːdɪŋ ˈzɪərəʊ, ænd ðə rɪˈmeɪnɪŋ 0 ɑː ˈpleɪsˌhəʊldəz.
 
+
+
+
+
 <eng>
 In This Chapter
 - Understanding how place_value turns digits into numbers
@@ -160,5 +139,30 @@ With a really strong foundation in these areas, you'll find the chapters that fo
 aɪ bɛt juː nəʊ ə lɒt ɒv ðɪs stʌf ɔːlˈrɛdi.
 səʊ kənˈsɪdə ðɪs ə trɪp daʊn ˈmɛməri leɪn, ə ˈmɪnɪ-vəˈkeɪʃən frɒm wɒtˈɛvə mæθ juː meɪ biː ˈwɜːkɪŋ ɒn raɪt naʊ.
 wɪð ə ˈrɪəli strɒŋ faʊnˈdeɪʃən ɪn ðiːz ˈeərɪəz,  juːl faɪnd ðə ˈʧæptəz ðæt ˈfɒləʊ ə lɒt ˈiːzɪə.
+
+<eng>
+First, I discuss how the number system you’re familiar with - called the Hindu-Arabic number system (or decimal numbers)  - uses digits and place_value to express numbers.
+Next, I show you how to round numbers to the nearest ten, hundred, or thousand.
+After that, I discuss the Big Four operations: adding, subtracting, multiplying, and dividing.
+You see how to use the number line to make_sense_of all four operations.
+Then I give you practice doing calculations with larger numbers.
+To finish up, I make_sure you know how to_do long division both with and without a remainder.
+Algebra often uses the dot (.) in_place_of the times sign (x) to indicate multiplication, so that’s what I use in this book.
+<rus>
+Сначала, я расскажу как система счисления с которой вы знакомы - она называется индуистско-арабской системой счисления (или десятичными числами) - использует цифры и поместное_значение для выражения чисел.
+Далее я покажу вам, как округлять числа до ближайших десятков, сотен, или тысяч.
+После этого я расскажу про операции Большой Четверки: сложение, вычитание, умножение и деление.
+Вы увидите как использовать числовую строку чтобы_понять все четыре операции.
+Затем я дам вам практику выполнения расчетов с большими числами.
+Чтобы закончить, я должен_быть_уверен_что вы знаете как сделать длинное деление как с остатком так и без него.
+Алгебра часто использует точку (.) Вместо знака времени (x) для обозначения умножения, и именно это я и использую в этой книге.
+<trn>
+fɜːst, aɪ dɪsˈkʌs haʊ ðə ˈnʌmbə ˈsɪstɪm jʊə fəˈmɪliə wɪð — kɔːld ðə ˈhɪnˈduː-ˈærəbɪk ˈnʌmbə ˈsɪstɪm (ɔː ˈdɛsɪməl ˈnʌmbəz) — ˈjuːzɪz ˈdɪʤɪts ænd pleɪs ˈvæljuː tuː ɪksˈprɛs ˈnʌmbəz.
+nɛkst, aɪ ʃəʊ juː haʊ tuː raʊnd ˈnʌmbəz tuː ðə ˈnɪərɪst tɛn, ˈhʌndrəd, ɔː ˈθaʊzənd.
+ˈɑːftə ðæt, aɪ dɪsˈkʌs ðə bɪg fɔːr ˌɒpəˈreɪʃənz: ˈædɪŋ, səbˈtræktɪŋ, ˈmʌltɪplaɪɪŋ, ænd dɪˈvaɪdɪŋ.
+juː siː haʊ tuː juːz ðə ˈnʌmbə laɪn tuː meɪk sɛns ɒv ɔːl fɔːr ˌɒpəˈreɪʃənz.
+ðɛn aɪ gɪv juː ˈpræktɪs ˈdu(ː)ɪŋ ˌkælkjʊˈleɪʃənz wɪð ˈlɑːʤə ˈnʌmbəz.
+tuː ˈfɪnɪʃ ʌp, aɪ meɪk ʃʊə juː nəʊ haʊ tuː duː lɒŋ dɪˈvɪʒən bəʊθ wɪð ænd wɪˈðaʊt ə rɪˈmeɪndə.
+ˈælʤɪbrə ˈɒf(ə)n ˈjuːzɪz ðə dɒt (.) ɪn pleɪs ɒv ðə taɪmz saɪn (ɛks) tuː ˈɪndɪkeɪt ˌmʌltɪplɪˈkeɪʃən, səʊ ðæts wɒt aɪ juːz ɪn ðɪs bʊk.
 
 `]);
