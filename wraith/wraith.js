6b2529ef95de41dcfb5e46f3600ce8c60ed25493
Gurko Ilya <blues74@mail.ru>
2020-05-31 22:48:12 +0700

    noop

---
+++ wraith/wraith.js
@@ -0,0 +1,311 @@
+TEXTS.push([`
+
+https://www.short-story.me/stories/fantasy-stories
+https://www.theazrianportal.com/the-wraith-fantasy-short-story
+
+<eng>
+“How does it look?”
+
+Carefully, I gripped my companion by the shoulder and pulled him away from the slippery stone wall.
+He left behind stains of blood on the cobalt rock; strands, thick and black, sticking to where he once rested.
+In the flickering firelight, what little I could see was pure horror.
+Three deep gashes, each about three inches apart, were scored across his back from left waist to right shoulder.
+A mess of torn flesh, muscle and tendons, he had been ripped to the bone, right through his thick furs and leathers.
+In the freezing temperatures, the blood seemed to flow less willingly, but this made the injury no less grim.
+Slowly, I let him roll back to his resting place against the wall of the cave.
+The heat from the fire warmed the rock that encased us, forming droplets of water that either dripped from the jagged ceiling or slid their way down the curved wall.
+I wiped my hands on the moisture, trying to remove some of the blood that coated them.
+
+<rus>
+"Как это выглядит?"
+
+Осторожно я схватил своего спутника за плечо и оттащил его от скользкой каменной стены.
+Он оставил пятна крови на кобальтовой скале; пряди, толстые и черные, придерживаясь того места, где он когда-то отдыхал.
+В мерцающем свете костра то, что я мог видеть, было чистым ужасом.
+Три глубоких пореза, каждый на расстоянии около трех дюймов, были нанесены по его спине от левой талии до правого плеча.
+Грязь из разорванной плоти, мышц и сухожилий, он был разорван до костей, прямо сквозь толстые меха и кожу.
+При низких температурах кровь, казалось, текла реже, но это делало травму не менее мрачной.
+Медленно я позволил ему откатиться к месту отдыха у стены пещеры.
+Тепло от огня согревало скалу, которая нас окружала, образуя капли воды, которые либо капали с неровного потолка, либо скользили по изогнутой стене.
+Я вытер руки о влагу, пытаясь удалить кровь, покрывавшую их.
+<trn>
+
+
+<eng>
+“I’ve seen worse”.
+“On a gutted pig?” He chuckled breathlessly, his voice rattling.
+“You just need rest. The wound will heal enough by morning.”
+He gave another, hollow laugh and smiled, a crooked, disbelieving smile.
+“You make it pay, won’t you?”
+“We will.” I insisted, putting my hand on his shoulder.
+<rus>
+«Я видел хуже».
+«На потрошеной свинье?» Он хмыкнул, затаив дыхание.
+«Вам просто нужен отдых. Рана достаточно заживет к утру.
+Он еще раз пусто рассмеялся и улыбнулся кривой, не верящей улыбкой.
+«Вы платите, не так ли?»
+"Мы будем." Я настоял, положив руку ему на плечо.
+<trn>
+
+<eng>
+I looked the dying man in the eye, hoping that some of my confidence would inspire him to keep on fighting.
+He just gazed back, his eyelids drooping.
+I could tell he was tired.
+So very, very tired.
+He didn’t seem to have the energy to fight, or to even care that he was facing his last moments.
+Harsh winds wailed past the entrance to the cave.
+A blizzard was tearing through the night beyond our little shelter.
+I could feel the bitterness of the frozen world outside.
+The dwindling fire a meagre offering compared to the unrelenting nightmare of snow; little protection against the sharp, icy winds that crept their way into our abode.
+<rus>
+Я посмотрел умирающему в глаза, надеясь, что часть моей уверенности вдохновит его продолжать сражаться.
+Он просто смотрел назад, его веки поникли.
+Я мог сказать, что он устал.
+Так очень, очень устал.
+У него, казалось, не было сил сражаться или даже волноваться о том, что он столкнулся со своими последними моментами.
+Резкие ветры пронеслись мимо входа в пещеру.
+Снежная буря разрывала ночь за нашим маленьким укрытием.
+Я чувствовал горечь замерзшего мира снаружи.
+Обжигающий огонь скудный жертва по сравнению с неумолимым кошмаром снега; маленькая защита от острых, ледяных ветров, которые проникали в нашу обитель.
+<trn>
+
+<eng>
+By the time dawn broke, I was huddled in the deepest corner of the cave, wrapped tightly in the fur cloak I had thought would keep me warm in these unforgiving mountains.
+My companion lay dead, taken either by his wounds or the ice that encrusted his lips and eyelashes.
+The fire had died about the same time he did.
+So long ago that it no longer even smouldered.
+The blizzard had cleared, but snow still fell.
+It almost never stopped falling in the mountains.
+In a stupor of hunger and sleep deprivation, I emerged from the cave.
+The sun had only just risen above the towering mountain peaks, but shone so brightly between the gaps of grey cloud that swarmed overhead I was nearly blinded.
+Its rays streamed off every flake of glossy white snow, snow that coated everything in sight; from giant boulders jutting from the canyon walls, to the trees that lined the deep valley sprawling away over both of my shoulders.
+<rus>
+Когда наступил рассвет, меня сгрудили в самом глубоком углу пещеры, плотно завернув в меховой плащ, который, как я думал, согреет меня в этих неумолимых горах.
+Мой компаньон лежал мертвым, забитый его ранами или льдом, покрывавшим его губы и ресницы.
+Огонь умер примерно в то же время, что и он.
+Так давно, что он уже не тлеет.
+Метель рассеялась, но снег все еще падал.
+Он почти никогда не переставал падать в горы.
+В ступоре голода и лишения сна я вышел из пещеры.
+Солнце только что поднялось над высокими горными вершинами, но сияло так ярко между промежутками серого облака, которое парило над головой, что я был почти ослеплен.
+Его лучи стекали с каждой чешуйки глянцевого белого снега, снега, который покрывал все, что было видно; от гигантских валунов, выступающих от стен каньона, до деревьев, которые выровняли глубокую долину, раскинувшуюся на обоих моих плечах.
+<trn>
+
+<eng>
+We’d found our hideaway halfway up the side of the valley the night before.
+As night fell, a lucky break in the barrage of snow meant the dark rocky archway caught my eye,
+a striking contrast against the white world that surrounded it.
+In a rush of cries, blood and panic, we’d scrambled inside.
+There was no sign of that now.
+No sign of the frantic night before.
+The snow made everything so elegant; so calm; so clean.
+I had no idea where I was going now.
+I had no idea what I was going to do.
+The man that lay dead inside the cave was the smart one; my problem solver.
+The man that lay dead somewhere in the snow before me was my navigator; the one who could tell me where to go.
+I was just the muscle.
+The farmhand with a large sword and the will to swing it.
+
+And I was alone.
+<rus>
+Мы нашли наше убежище на полпути вверх по склону долины прошлой ночью.
+Когда наступила ночь, счастливый прорыв в снежной гряде означал, что темная скалистая арка привлекла мое внимание,
+разительный контраст с белым миром, который его окружал.
+В порыве криков, крови и паники мы забрались внутрь.
+Там не было никаких признаков этого сейчас.
+Никаких признаков безумной ночи раньше.
+Снег сделал все так элегантно; так спокойно; так чисто.
+Я понятия не имел, куда я иду сейчас.
+Я понятия не имел, что я собирался делать.
+Человек, который лежал мертвым внутри пещеры, был умным; мой решатель проблем.
+Человек, который лежал мертвым где-то на снегу передо мной, был моим штурманом; тот, кто мог сказать мне, куда идти.
+Я был просто мышцей.
+Ферма с большим мечом и волей его размахивать.
+
+И я был один.
+<trn>
+
+<eng>
+My father had always warned me about coming into the mountains.
+There were two truths in life, he used to say: That the mountains were no place for farm folk, and that Uncle Ryle was a no good swindler.
+I’d learnt as a boy that my Uncle was not a swindler, he was just better at business.
+But many years later, I was finally learning my father wasn’t wrong about everything.
+But I had a purpose for being here; we all had a purpose.
+And we knew there were risks.
+<rus>
+Мой отец всегда предупреждал меня о приходе в горы.
+Он говорил, что в жизни было две истины: в горах нет места фермерам, а дядя Райл не был хорошим мошенником.
+В детстве я узнал, что мой дядя не был мошенником, он просто был лучше в бизнесе.
+Но много лет спустя я наконец узнал, что мой отец не ошибался во всем.
+Но у меня была цель быть здесь; у всех нас была цель.
+И мы знали, что есть риски.
+<trn>
+
+<eng>
+There was nothing left for me now.
+Nothing left but to finish what we started.
+I dug my boots into the snow beneath and trudged on.
+Where I was travelling — east, north, west, I had no idea — but I knew what I was looking for.
+I knew what I was hunting.
+Wraiths left behind a sort of luminous mist wherever they moved, like the shimmering trail of a slug.
+This was how we knew what had slaughtered the people of the village.
+This is how I knew what had killed my father.
+Often the trails meant it was easy to avoid the wraiths, but I wasn’t looking to avoid this one.
+<rus>
+Теперь мне ничего не осталось.
+Ничего не осталось, кроме как закончить то, что мы начали.
+Я вырыл свои ботинки в снег под ними и пошел дальше.
+Где я путешествовал - восток, север, запад, я понятия не имел - но я знал, что искал.
+Я знал, что я охотился.
+Призраки оставляли позади своего рода светящийся туман, куда бы они ни двигались, как мерцающий след слизняка.
+Так мы узнали, что убило жителей деревни.
+Так я узнал, что убило моего отца.
+Часто следы означали, что легко было избежать призраков, но я не хотел этого избегать.
+<trn>
+
+<eng>
+We’d found it already, or I should say, it had found us.
+Its bright frosty-blue eyes had emerged from in the encircling blizzard last night, appearing as if from nowhere out of the gloom.
+I immediately took a swing and missed, but it didn’t.
+It took out my navigator with a slash off its left claw, falling away into the blizzard, lost in the night forever.
+In the same movement, the beast carved up my companion with the right claw.
+There was no chance to defend ourselves, no chance to fight after that.
+We just had to run.
+It had now taken everyone from me.
+The last survivors.
+My family, my home.
+I couldn’t even go back to the farm, not really.
+Growing food on the frozen plains that lay in the shadow of these hellish mountains was damn-near impossible.
+Getting the conditions right in the frigid temperatures, where ice never left the ground, took a lifetime of experience
+and my father had not yet managed to teach me all I needed to know.
+Twenty-two years apparently was still not enough to be a farmer in a world ruled by frost.
+All I had now was my goal, our goal.
+To kill the beast that stole my future.
+<rus>
+Мы уже нашли это, или я должен сказать, это нашло нас.
+Его яркие морозно-голубые глаза появились из окружавшей пурги прошлой ночью, словно из ниоткуда мрака.
+Я немедленно взмахнул рукой и пропустил, но это не так.
+Он вырвал мой штурман, ударив его левой когтью, и упал в метель, потерянную вечно ночью.
+В том же движении зверь вырезал моего спутника правым когтем.
+Не было ни шанса защитить себя, ни шансов драться после этого.
+Мы просто должны были бежать.
+Теперь все забрали меня.
+Последние выжившие.
+Моя семья, мой дом.
+Я даже не мог вернуться на ферму, не совсем.
+Выращивание пищи на замерзших равнинах, лежащих в тени этих адских гор, было чертовски почти невозможно.
+Правильные условия при холодных температурах, когда лед никогда не покидал землю, потребовали опыта на всю жизнь,
+и мой отец еще не успел научить меня всему, что мне нужно было знать.
+Двадцать два года явно было недостаточно, чтобы быть фермером в мире, управляемом морозом.
+Все, что у меня было сейчас, это моя цель, наша цель.
+Чтобы убить зверя, который украл мое будущее.
+<trn>
+
+<eng>
+We weren’t even sure why it was in the village.
+Not in my lifetime, nor my father’s, nor his father’s before him, had a wraith left the mountains.
+He used to tell me all about them, as the hearth crackled away.
+The stories had grown even longer of late, with the new mining going on in the mountains.
+They brought to the village some new stone fuel, and with it, the fire burned longer than ever — which meant the stories just kept on going.
+Stories of monstrous creatures in the mountains.
+Bloodthirsty demons of an old world.
+My father would regale me with tales of wraiths cutting down weary travellers and seasoned knights alike.
+He himself had never seen one, nor had anyone I knew, but we all knew what they were.
+The stories were as old as time.
+The miner’s work got me asking even more questions about my father’s rules, though.
+Why were they allowed into the mountains if they were so dangerous?
+Apparently, a combination of military convoys and fire-wielding sorceresses kept them safe from both the elements and the wraiths.
+I was a bit disgruntled with the notion that I was so incapable when they were not.
+Those feelings had long since passed.
+<rus>
+Мы даже не были уверены, почему это было в деревне.
+Ни в моей жизни, ни у моего отца, ни у его отца до него не было духа покинуть горы.
+Раньше он рассказывал мне все о них, когда очаг потрескивал.
+Истории выросли еще дольше, с новой добычей в горах.
+Они привезли в деревню какое-то новое каменное топливо, и вместе с этим огонь горел дольше, чем когда-либо, что означало, что истории только продолжаются.
+Истории о чудовищных существах в горах.
+Кровожадные демоны старого мира.
+Мой отец угощал меня рассказами о призраках, которые вырубали усталых путешественников и бывалых рыцарей.
+Сам он никогда не видел никого, никого, кого я знал, но мы все знали, кто они.
+Истории были так же стары, как и время.
+Работа шахтера заставила меня задавать еще больше вопросов о правилах моего отца.
+Почему их пустили в горы, если они были такими опасными?
+По-видимому, комбинация военных конвоев и огненных колдун защищала их как от стихий, так и от призраков.
+Я был немного недоволен тем, что я был так неспособен, когда их не было.
+Эти чувства давно прошли.
+<trn>
+
+
+<eng>
+I wandered through the frost-touched valley for what seemed like an eternity.
+The land was beautiful, but baron.
+There was no food, no warmth, and no life, save the snow-laden pine trees that reached on for miles.
+I was feeling weaker and weaker by the minute.
+I had lost our supplies in the rush the previous night.
+The pack of food was now buried under inches of snow.
+I knew I’d never find it.
+I didn’t bother to search.
+Between dazzling brightness and dreary gloom, depending on how covered by clouds the sun was, I started to feel my resolve wain.
+I wanted to quit, to give up and leave this glorious, deadly place behind.
+But I couldn’t.
+I thought of the mangled bodies of my family.
+The guilt was overwhelming.
+Had I been there, not out in the city chasing women, I might have been able to fend off the beast.
+<rus>
+Я бродил по замерзшей долине, казалось, целую вечность.
+Земля была прекрасна, но барон.
+Не было ни еды, ни тепла, ни жизни, кроме заснеженных сосен, которые тянулись на многие мили.
+С каждой минутой я чувствовал себя все слабее и слабее.
+Я потерял наши запасы в спешке прошлой ночью.
+Пакет с едой был теперь похоронен под дюймами снега.
+Я знал, что никогда не найду это.
+Я не стал искать.
+Между ослепительной яркостью и мрачным унынием, в зависимости от того, насколько облачно было покрыто солнце, я начал чувствовать, что моя решимость ослабла.
+Я хотел уйти, сдаться и оставить это славное, смертельное место позади.
+Но я не мог.
+Я думал о искалеченных телах моей семьи.
+Вина была подавляющей.
+Если бы я был там, а не в городе в погоне за женщинами, я бы смог отразить зверя.
+<trn>
+
+<eng>
+The village wasn’t home to many able young men, only three, and none had been around when it came.
+Then I thought of my companion in the cave, the frost surely still biting at his quickly freezing corpse.
+The guilt was worse still.
+As they wept over their families, my heart filled with rage.
+I felt the anger wash over me, masking the pain — I knew that now, as the cold bit at my own heart and began to thaw the fiery anger that burned inside.
+I had to almost drag our navigator into the mountains, and now he lay dead within them.
+My companion was only slightly more willing.
+The wraith took both his wife and daughter, but even he wouldn’t have set foot on these treacherous slopes if I hadn’t driven him to it.
+I goaded him, playing off his torment.
+I had to kill the beast, it was the only way to unburden my guilty conscience.
+<rus>
+В деревне не было много способных молодых людей, только трое, и никого не было рядом, когда он пришел.
+Затем я подумал о своем спутнике в пещере, мороз наверняка все еще кусал его быстро замерзший труп.
+Вина была еще хуже.
+Когда они оплакивали свои семьи, мое сердце наполнилось яростью.
+Я почувствовал, как гнев нахлынул на меня, маскируя боль - теперь я знал это, когда холод у меня в душе, и я начал размораживать огненный гнев, который горел внутри.
+Мне пришлось почти затащить нашего штурмана в горы, и теперь он лежал мертвым в них.
+Мой собеседник был чуть более охотно.
+Призрак забрал и его жену и дочь, но даже он не ступил бы на эти коварные склоны, если бы я не довел его до него.
+Я уговорил его, разыгрывая его мучения.
+Я должен был убить зверя, это был единственный способ освободить мою совесть.
+<trn>
+
+<eng>
+<rus>
+<trn>
+
+<eng>
+<rus>
+<trn>
+
+<eng>
+<rus>
+<trn>
+
+<eng>
+<rus>
+<trn>
+
+`]);
