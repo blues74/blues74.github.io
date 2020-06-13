0e44ae03211988982aaff65136c77ed7cdcd3537
Gurko Ilya <blues74@mail.ru>
2020-06-13 13:32:03 +0700

    noop

--- texts/words.js
+++ texts/words.js
@@ -1,4 +1,26 @@
+// ☰
 TEXTS.words.push([`
+
+    `,`
+[AB] [words]
+
+It's important to keep abreast of the latest developments in computers.
+Важно быть в курсе последних событий в области компьютерных технологий.
+
+As the car drew abreast of him, Jack suddenly recognised the driver.
+Когда автомобиль поравнялся с Джеком, он неожиданно узнал водителя.
+
+A pass so narrow that but two carts could go abreast along it.
+Проезд настолько узок, что вдоль него могут двигаться только две повозки.
+
+The planes were flying four abreast.
+Самолеты летели по четыре в ряд.
+
+Jim was abreast of the leading runner for a few minutes but then fell behind.
+Сначала Джим бежал наравне с лидером, но потом отстал.
+
+You will have to run fast to remain abreast of our best runner.
+Тебе нужно бежать очень быстро, чтобы не отставать от лидера.
 
     `,`
 
