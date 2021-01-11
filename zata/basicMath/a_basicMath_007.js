4c309a9e869276196d369e6c942d947c12737152
Gurko Ilya <blues74@mail.ru>
2021-01-11 16:12:02 +0300

    math

--- zata/basicMath/a_basicMath_007.js
+++ zata/basicMath/a_basicMath_007.js
@@ -91,27 +91,4 @@ hundreds digit --- hʌndrədz dɪʤɪt --- разряд сотен --- hʌndrəd
 tens digit --- tɛnz dɪʤɪt --- разряд десятков --- tɛnz dɪʤɪt
 ones digit --- wʌnz dɪʤɪt --- разряд единиц --- wʌnz dɪʤɪt
 
-      `,`
-
-[010] [basicMath] [consonant_sounds]
-rounding --- raʊndɪŋ --- округление --- raʊndɪŋ
-two-digit number --- tu-dɪʤət nʌmbər --- двузначное число --- tu-dɪʤət nʌmbər
-simply --- sɪmpli --- просто --- sɪmpli
-bring_up --- brɪŋ_ʌp --- поднимать, растить, взрастить, выкармливать --- brɪŋ_ʌp
-bring_down --- brɪŋ_daʊn --- снижать, сбивать, сваливать --- brɪŋ_daʊn
-turn --- tɜrn --- превращать, повернуть --- tɜrn
-
-      `,`
-
-[term] [basicMath] [consonant_sounds]
-a'ddition --- ə'dɪʃn --- сложение --- ə'dɪʃn
-sub'traction --- səb'trækʃn --- вычитание --- səb'trækʃn
-'multipli'cation --- mʌltɪplɪ'keɪʃn --- умножение --- mʌltɪplɪ'keɪʃn
-di'vision --- dɪ'vɪʒ(ə)n --- деление --- dɪ'vɪʒ(ə)n
-vector --- vektə(r) --- вектор --- vektə(r)
-colline'arity --- kəʊlini'æriti --- коллинеарность (лежание на одной прямой) --- kəʊlini'æriti
-co'llinear --- kəu'lɪnɪə ---  лежащий на одной прямой, коллиниарный --- kəu'lɪnɪə
-
-    `,`
-
 `]);
---
+++ zata/basicMath/a_basicMath_010.js
@@ -0,0 +1,26 @@
+ALL_DATA.push([`
+
+[010] [basicMath] [consonant_sounds]
+
+rounding --- raʊndɪŋ --- округление --- raʊndɪŋ
+two-digit number --- tu-dɪʤət nʌmbər --- двузначное число --- tu-dɪʤət nʌmbər
+simply --- sɪmpli --- просто --- sɪmpli
+bring_up --- brɪŋ_ʌp --- поднимать, растить, взрастить, выкармливать --- brɪŋ_ʌp
+bring_down --- brɪŋ_daʊn --- снижать, сбивать, сваливать --- brɪŋ_daʊn
+turn --- tɜrn --- превращать, повернуть --- tɜrn
+
+      `,`
+
+[term] [basicMath] [consonant_sounds]
+
+a'ddition --- ə'dɪʃn --- сложение --- ə'dɪʃn
+sub'traction --- səb'trækʃn --- вычитание --- səb'trækʃn
+'multipli'cation --- mʌltɪplɪ'keɪʃn --- умножение --- mʌltɪplɪ'keɪʃn
+di'vision --- dɪ'vɪʒ(ə)n --- деление --- dɪ'vɪʒ(ə)n
+vector --- vektə(r) --- вектор --- vektə(r)
+colline'arity --- kəʊlini'æriti --- коллинеарность (лежание на одной прямой) --- kəʊlini'æriti
+co'llinear --- kəu'lɪnɪə ---  лежащий на одной прямой, коллиниарный --- kəu'lɪnɪə
+
+    `,`
+
+`]);
